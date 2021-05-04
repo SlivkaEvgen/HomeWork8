0962fecace488e6ee8a9174dacd6d584d7125aa7
SlivkaEvgen <79520449+SlivkaEvgen@users.noreply.github.com>
2021-05-04 07:36:15 +0300

    Delete src directory

--- src/MyArrayList/MyList1.java
+++
@@ -1,9 +0,0 @@
-package MyArrayList;
-
-public interface MyList1<E> {
-    void add(E element); //добавляет элемент в конец
-    void remove(int index); //удаляет элемент под индексом
-    void clear();           //очищает коллекцию
-    int size();             //возвращает размер коллекции
-    E get(int index);       //возвращает элемент под индексом
-}
--- src/MyHashMap/InterHashMap.java
+++
@@ -1,9 +0,0 @@
-package MyHashMap;
-
-public interface InterHashMap<E,T> {
-    void put(Object key, Object value);
-    void remove(Object key);
-    void clear();
-    int size();
-    T get(E key);
-}
